f394099df52fb01f0f53bf9ffaea0af29df1d3ac
eddygld <61808648+eddygld@users.noreply.github.com>
2021-01-18 18:27:50 -0500

    gifs now display on cards

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
 
 const Gif = (props) => {
     return (
         <div>
-            <img src={props.imgUrl} alt="" />
+            <Card>
+                <CardMedia style={{height: 0, paddingTop: '56.25%'}} image={props.imgUrl} >
+
+                </CardMedia>
+            </Card>
+            {/* <img src={props.imgUrl} alt="" /> */}
         </div>
     )
 }
